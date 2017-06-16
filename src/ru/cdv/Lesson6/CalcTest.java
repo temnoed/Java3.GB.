8c881cc4b97616cb7161164e26882df4cacd7bad
temnoed <dc9257351633@gmail.com>
2017-06-16 21:57:07 +0300

    lesson6. DZ. task1. Тесты.

---
+++ src/ru/cdv/Lesson6/CalcTest.java
@@ -0,0 +1,36 @@
+package ru.cdv.Lesson6;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcTest {
+    private Calculator calculator;
+
+    @Before
+    public void startTest() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    public void testSub() {
+        Assert.assertEquals(3, calculator.sub(5, 2));
+    }
+
+    @Test
+    public void testMul() {
+        Assert.assertEquals(9, calculator.mul(3, 3));
+    }
+
+    @Test
+    public void testDiv() {
+        Assert.assertEquals(1, calculator.div(2, 2));
+    }
+}
+
--- src/ru/cdv/Lesson6/MainDz6.java
+++ src/ru/cdv/Lesson6/MainDz6.java
@@ -1,9 +1,45 @@
 package ru.cdv.Lesson6;
 
-/**
- * Created by Admin on 11.06.17.
- */
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class MainDz6 {
+
+    private static MainDz6 mainObj = new MainDz6();
+    private int[] theArr;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(mainObj.task1(1, 2, 3, 4, 5, 6, 7)));
+
+    }
+
+    @Test
+    public void testTask1() {
+        int assert1[] = {5, 6};
+        Assert.assertEquals(assert1, mainObj.task1(1, 2, 3, 4, 5, 6, 7));
+    }
+
+
+    private Integer[] task1(Integer... arr) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arr));
+        if (!list.contains(4)) throw new RuntimeException("В массиве нет четвёрки!");
+
+        int index = list.indexOf(4);
+        Integer[] result = new Integer[arr.length - index - 1];
+
+        for (int i = 0; i < arr.length - index - 1; i++) {
+            result[i] = arr[index + i + 1];
+        }
+
+        return result;
+    }
 
 
 }
